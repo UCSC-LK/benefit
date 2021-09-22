1acad572310f7d70fa4f04f862163f7e322ef415
LakshaniNadeesha <83568940+LakshaniNadeesha@users.noreply.github.com>
2021-09-22 11:03:40 +0530

    Add HR Officer Header File

---
+++ srs/views/includes/hrofficerheader.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?=CSS_PATH?>hrofficerheader.css" >
+    <title></title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="header">
+        <div class="nav-bar">
+            <ul>
+                <li><a href="#">Register Employee</a></li>
+                <li><a href="#">Remove Employee</a></li>
+                <li><a href="#">Update Documents</a></li>
+                <li><a href="#">Update Benefits</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
