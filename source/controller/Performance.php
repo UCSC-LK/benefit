13dba3e9d5a2bf620d4eb805ea2dcf3d56af9e6e
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-09-29 16:24:06 +0530

    Update Performance.php

--- source/controller/Performance.php
+++ source/controller/Performance.php
@@ -8,36 +8,17 @@ class Performance extends Controller
 	
 	function index()
 	{
-		// code...
-		//echo "<pre>";
-		//print_r($_POST);
-		$errors=array();
-		
-		if(count($_POST)>0)
+
+		if(!Auth::logged_in())
 		{
-			$user=new PerformanceModel();
-			//($user->validate($_POST)
-			if(isset($_POST['submit']))
-			{
-				
-				$arr['employee_ID']=$_POST['employee_ID'];
-				$arr['communication']=$_POST['communication'];
-				$arr['quality_of_work']=$_POST['quality_of_work'];
-				$arr['organization']=$_POST['organization'];
-				$arr['team_skills']=$_POST['team_skills'];
-				$arr['multitasking_ability']=$_POST['multitasking_ability'];
-				$user->insert($arr);
-				//redirect
-				$this->redirect('home');
-			}
-		//	else
-		//	{
-				//err
-		//	$errors= $user->errors;
-		//	}
+			$this->redirect('login');
 		}
-		//$this->view('performance',['errors'=>$errors]);
-		$this->view('performance');
+		$user=new PerformanceModel();
+		$ar=Auth::user();
+		$row=array();
+		$row = $user->where('employee_ID',$ar);
+		$this->view('performance',['row'=>$row]);
 		
 	}
 }
+
