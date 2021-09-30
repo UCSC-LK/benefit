15a179650520747ec1b3eeb5e4cbce4728b08858
Dilukshan Bimsara <77828545+dilukshanbimsara@users.noreply.github.com>
2021-09-30 05:30:41 +0530

    Add Employee List Controller

---
+++ source/controller/EmployeelistController.php
@@ -0,0 +1,42 @@
+<?php
+
+class EmployeelistController extends Controller{
+
+    function index(){
+
+        $user = new EmployeelistModel();
+
+        $data = $user->where('department_ID',2);
+
+        // if(count($_POST)>0){
+
+            if(isset($_POST['edit'])){
+                // $arr['employee_ID'] = $_POST['id'];
+
+                
+                session_start(); 
+                   
+
+                $_SESSION['id'] = $_POST['id'];
+                // echo $_POST['id'];
+
+                $this->redirect('UpdateemployeeController');
+            }
+
+            if(isset($_POST['delete'])){
+                // $arr['employee_ID'] = $_POST['id'];
+                if(!isset($_SESSION)) { 
+                    session_start(); 
+                } 
+
+                $_SESSION['id'] = $_POST['id'];
+
+                $this->redirect('#');
+            }
+
+        // }
+
+        
+        $this->view('employeelist',['rows'=>$data]);
+    }
+}
