5934c97297fb6ba22a596aeeb5854a105cc1f527
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2021-09-30 13:29:11 +0530

    Add Navigation View and CSS

---
+++ source/views/includes/navigation.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?=CSS_PATH?>navigation.css">
+
+    <title></title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="header">
+        <div class="nav-bar">
+            <ul>
+                <li><a href="#">Approve Leaves</a></li>
+                <li><a href="#">Approve Reimbursement</a></li>
+                <li><a href="#">Add Performance</a></li>
+                <li><a href="#">Mark Attendance</a></li>
+                <li><a href="#">Register Employee</a></li>
+                <li><a href="#">Remove Employee</a></li>
+                <li><a href="#">Update Employee Details</a></li>
+                <li><a href="#">Approve Benefits</a></li>
+                <li><a href="#">Update Documents</a></li>
+                <li><a href="#">Update Benefits</a></li>
+                <li><a href="#">Handle Hierarchy</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
