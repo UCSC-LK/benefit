4e7c171760e36e103fe0a578a318d618a089b9ef
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-09-30 21:05:19 +0530

    Update Addperformance.php

--- source/controller/Addperformance.php
+++ source/controller/Addperformance.php
@@ -8,20 +8,36 @@ class Addperformance extends Controller
 	
 	function index()
 	{
-
-		// if(!Auth::logged_in())
-		// {
-		// 	$this->redirect('login');
-		// }
-		$user=new PerformanceModel();
-		$ar=Auth::user();
-		$row=array();
-		$row = $user->where('employee_ID',$ar);
-		$this->view('addperformance',['row'=>$row]);
+		// code...
+		//echo "<pre>";
+		//print_r($_POST);
+		$errors=array();
+		
+		if(count($_POST)>0)
+		{
+			$user=new PerformanceModel();
+			//($user->validate($_POST)
+			if(isset($_POST['submit']))
+			{
+				
+				$arr['employee_ID']=$_POST['employee_ID'];
+				$arr['communication']=$_POST['communication'];
+				$arr['quality_of_work']=$_POST['quality_of_work'];
+				$arr['organization']=$_POST['organization'];
+				$arr['team_skills']=$_POST['team_skills'];
+				$arr['multitasking_ability']=$_POST['multitasking_ability'];
+				$user->insert($arr);
+				//redirect
+				$this->redirect('home');
+			}
+		//	else
+		//	{
+				//err
+		//	$errors= $user->errors;
+		//	}
+		}
+		//$this->view('performance',['errors'=>$errors]);
+		$this->view('addperformance');
 		
 	}
-
-	
-
-
 }
