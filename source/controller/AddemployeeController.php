1fb0293b12bf4008b2fef88c05680119d9061ea7
Dilukshan Bimsara <77828545+dilukshanbimsara@users.noreply.github.com>
2021-10-03 14:34:43 +0530

    Add Employee List To AddemployeeController.php

--- source/controller/AddemployeeController.php
+++ source/controller/AddemployeeController.php
@@ -8,18 +8,26 @@ class AddemployeeController extends Controller
 	
 	function index()
 	{
-		// code...
-		// echo "<pre>";
-		// print_r($_POST);
-		// $errors=array();
-		$err = array("Successfully added");
-		// $err = array(0,0,0,0);
+		$select = new AddemployeeModel();
+		$oper = $select->where('department_ID',1);
+		$hr = $select->where('department_ID',2);
+		$sells = $select->where('department_ID',3);
+		$acc = $select->where('department_ID',4);
+		$arr1 = array($oper,$hr,$sells,$acc);
+		for ($x = 4; $x < 10; $x++) {
+			$arr1[$x] = null;
+		  }
+		// $this->view('addemployee',['rows'=>$arr1]);
+
+
 		if(count($_POST)> 0)
 		{
 			$user=new AddemployeeModel();
+			// $user = new AddemployeeModel();
+			// $data = "define";
 			//($user->validate($_POST)
-			
 			if(isset($_POST['submit']))
+			// if(!empty($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST')
 			{
 				
 				$arr['first_name'] = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
@@ -32,93 +40,100 @@ class AddemployeeController extends Controller
 				$arr['marital_status'] = $_POST['marital'];
 				$arr['gender'] = $_POST['gender'];
 				$arr['contact_number'] = $_POST['contact'];
-
 				$arr['email'] =  filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-
 				$validate = $this->email_validate($arr['email'],$user);
+				
 
-
-				$pass=$_POST['pwd'];
-				$arr['password'] = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-				$pas = $_POST['confirm'];
-
-				// if($pas != $pass){
-				// 	array_push($err ,"password mismatch");
-				// }
-
+				$pass = $_POST['pwd'];
+				$arr['password'] = password_hash($pass,PASSWORD_DEFAULT);
+				$confirm = $_POST['confirm'];
 				$arr['user_role'] = filter_input(INPUT_POST, 'user_role', FILTER_SANITIZE_STRING);
 				$arr['supervisor_ID'] = $_POST['supervisor'];
+
+				$str['user_role'] = "Supervisor";
+
+				// $user->update($_POST['supervisor'],$str);
+
+				// if(isset($_POST['submit'])) {
+					//submit button is clicked
+					$file = $_FILES['image'];
 			
-				$file = $_FILES['image'];
-		
-				$file_name = $_FILES['image']['name'];
-				$file_type = $_FILES['image']['type'];
-				$file_size = $_FILES['image']['size'];
-				$file_error = $_FILES['image']['error'];
-				$file_temp_name = $_FILES['image']['tmp_name'];
-		
-				$file_ext = explode('.',$file_name);
-				$file_actual_ext = strtolower(end($file_ext));
-		
-				$allowed = array('jpg','jpeg','png','fifi'); //limit file types
-		
-				// echo "inside image upload";
-		
-				if (in_array($file_actual_ext, $allowed)) {
-					if ($file_error === 0) {
-		
-						if ($file_size < 5000000){ //set max size of image 500 KB
-		
-							$file_name_new = $arr['first_name']. "." .$file_actual_ext; //Rename profile picture with employee id
-							$file_designation =  'public/img/profile/' .$file_name_new;
-							$complete = move_uploaded_file($file_temp_name, $file_designation);
-							
-							$arr['profile_image'] = $file_designation;
-							// move_uploaded_file($file_temp_name, $nic.".".$file_actual_ext);
-		
+					$file_name = $_FILES['image']['name'];
+					$file_type = $_FILES['image']['type'];
+					$file_size = $_FILES['image']['size'];
+					$file_error = $_FILES['image']['error'];
+					$file_temp_name = $_FILES['image']['tmp_name'];
+			
+					$file_ext = explode('.',$file_name);
+					$file_actual_ext = strtolower(end($file_ext));
+			
+					$allowed = array('jpg','jpeg','png'); //limit file types
+			
+					// echo "inside image upload";
+			
+					if (in_array($file_actual_ext, $allowed)) {
+						if ($file_error === 0) {
+							if ($file_size < 500000){ //set max size of image 500 KB
+			
+								$file_name_new = $arr['first_name']. "." .$file_actual_ext; //Rename profile picture with employee id
+								$file_designation =  'public/img/profile/' .$file_name_new;
+								$complete = move_uploaded_file($file_temp_name, $file_designation);
+								
+								$arr['profile_image'] = $file_designation;
+								// $arr1[4] = 0;
+								// move_uploaded_file($file_temp_name, $nic.".".$file_actual_ext);
+			
+							}else {
+								$complete = false;
+								$arr1[4] = "Uploaded image too big";
+
+							}
 						}else {
-							// echo "your file too big!";
-							array_push($err , "your file too big!");
+							// echo "An error file cannot upload ";
+							$arr1[5] = "Image cannot upload try again!";
 						}
 					}else {
-						// echo "An error file cannot upload ";
-						array_push($err ,"An error file cannot upload ");
+						$arr1[6] = "You cannot upload files of this type";
+						
 					}
-				}else {
-					// echo "You cannot upload files of this type";
-					array_push($err ,"You cannot upload files of this type");
-				}
-		
+			
+				// }else{
+				// 	echo "something wrong";
+				// }
 				// $arr['profile_image'] = $_POST[];
 				$arr['designation_code'] = $_POST['designation'];
 				$arr['department_ID'] = $_POST['department'];
 
-				if ($validate){
-					// echo "email allready used";
-					array_push($err , "email all ready used");
-					$this->redirect('AddemployeeController');
-
-				}elseif ($complete && $pas === $pass){
+				// if($confirm != $pass){
+				// 	$arr1[7] = "Confirmed password not correct";
+				// 	// echo "Confirmed password not correct";
+				// }
+				if($validate){
+					$arr1[8] = "email is allready used!";
+				}
+				elseif($complete && $confirm == $pass ){
+					
 
 					$user->insert($arr);
+					$user->update($_POST['supervisor'],$str);
 
+					// $arr1[9] = "ff";
 					$this->redirect('AddemployeeRedirectController');
-
-				}else{
-					array_push($err ,"something wrong try again");
 				}
-			}			
+				else{
+					$arr1[9] = "Confirmed password not correct";
 					
+					// echo "upload welaa nee ";
+				}
+				
+			}
+	
 		}
 
-		if(count($err)>0){
-			$this->view('addemployee',['err'=>$err]);
-		}else{
-			$this->view('addemployee');
-		}
-		
-		// $this->view('addemployee');
-		
+
+		$this->view('addemployee',['rows'=>$arr1]);
+
+
 	}
 
 	function email_validate($email , $user){
