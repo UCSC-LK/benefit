861a6ab93784be43f437cbf013cb30bc166f54e7
Sathya <sathyaudayangi2@gmail.com>
2021-10-13 02:09:37 +0530

    Add Header2

---
+++ source/views/includes/header2.view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= CSS_PATH ?>header2.css">
+    <title></title>
+</head>
+<body>
+
+<div class="header2">
+    <nav class="stroke">
+        <ul>
+            <li><a href="<?= PATH ?>LeavedetailsController">Time Off</a></li>
+            <li><a href="<?= PATH ?>Reimbursement">Reimbursement</a></li>
+            <li><a href="<?= PATH ?>Benefit">Benefit</a></li>
+            <li><a href="<?= PATH ?>Performance">Performance</a></li>
+        </ul>
+    </nav>
+
+</div>
+
+</body>
+</html>
+
+
+
+
