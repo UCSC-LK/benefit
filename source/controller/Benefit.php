82fc545868b18951f8617673078f7fd292520d7c
Sathya <sathyaudayangi2@gmail.com>
2021-10-13 02:49:11 +0530

    Add Benefit Controller

---
+++ source/controller/Benefit.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ */
+class Benefit extends Controller
+{
+
+    function index()
+    {
+        if (!Auth::logged_in()) {
+            $this->redirect('login');
+        }
+        $user = new BenefitrequestModel();
+        $pending = array();
+        $ar = auth::user();
+        $pending = $user->where_condition('employee_ID','benefit_status', $ar ,'pending');
+
+        $this->view('benefit', ['pending' => $pending]);
+
+    }
+
+}
+
+
+
+
