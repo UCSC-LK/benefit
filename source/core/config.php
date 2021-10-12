cabbcded77d169d61456e9be03dbb20d2d5e8262
chathura <chathurabimalka@gmail.com>
2021-10-12 19:34:36 +0530

    Add config.php File

---
+++ source/core/config.php
@@ -0,0 +1,15 @@
+<?php
+
+
+define('PATH', 'http://localhost/benefit/');
+define('CSS_PATH', 'http://localhost/benefit/public/css/');
+define('IMG_PATH', 'http://localhost/benefit/public/img/');
+define('DOC_PATH', 'http://localhost/benefit/public/documents/');
+
+
+define('DBNAME', 'hrm');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBDRIVER', 'mysql');
+
