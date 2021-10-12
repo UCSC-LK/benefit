0389611a582f9049f824cbec7d42b3de94941363
Sathya <sathyaudayangi2@gmail.com>
2021-10-13 02:51:57 +0530

    Add Benefit Request Controller

---
+++ source/models/BenefitrequestModel.php
@@ -0,0 +1,11 @@
+<?php
+
+class BenefitrequestModel extends Model {
+
+    protected $table = "benefit_application";
+
+//    public function select_table($table_name){
+//        $table = $table_name;
+//    }
+
+}
