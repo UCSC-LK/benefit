34cc8444c7c8d46edfe27592c6d4ad420ceacf69
Dilukshan Bimsara <dilukshanbimsara@gmail.com>
2021-10-12 23:46:32 +0530

    Add Requestleave controller and Model

---
+++ source/controller/RequestleaveController.php
@@ -0,0 +1,10 @@
+<?php
+
+class RequestleaveController extends Controller{
+    function index(){
+        // $user = new RequestleaveModel();
+
+        $this->view('requestleave');
+    }
+    
+}
