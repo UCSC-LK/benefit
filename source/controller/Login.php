7a581509afccc3d6844f87580cfaccde332138f3
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-10-13 10:43:46 +0530

    Update Login.php

--- source/controller/Login.php
+++ source/controller/Login.php
@@ -43,4 +43,40 @@ class Login extends Controller
 		]);
 	}
 	
+	function forgotpassword()
+	{
+		if(isset($_POST['reset-req']))
+		{
+			$selector=bin2hex(random_bytes(8));
+			$token=random_bytes(32);
+			$hashedToken=password_hash($token, PASSWORD_DEFAULT);
+			/*$url="<?=PATH?>login/forgotpassword2?selector=".$selector."&validator=".bin2hex($token);*/
+			$url="localhost/benefit/Login/forgotpassword2?selector=".$selector."&validator=".bin2hex($token);
+			$expires=date("U") +1800;
+			$userEmail =$_POST["email"];
+			$user =new PwdResetModel();
+			$row=$user->deletepsw($userEmail);
+			$arr['pwdResetEmail']=$userEmail;
+			$arr['pwdResetSelector']=$selector;
+			$arr['pwdResetToken']=$hashedToken;
+			$arr['pwdResetExpires']=$expires;
+			$row=$user->insert($arr);
+			
+			$to=$userEmail;
+			$subject='Reset your password for mmtuts';
+			$massage= '<p>We recieved a password reset req.The link to reset your password </p>';
+			$massage.='<p>Here is your password reset link:</br>';
+
+			$massage.='<a href="'.$url.'">'.$url.'</a></p>';
+
+			$headers="From:mmtuts <chathurabimalka1997@gmail>\r\n";
+			$headers="Reply-To: chathurabimalka1997@gmail.com\r\n";
+			$headers="Content-type:text/html\r\n";
+			mail($to, $subject,$massage,$headers);
+			header("Location:forgotpassword?reset=success");
+
+		}
+		$this->view('forgotpassword');
+	}
+	
 }
