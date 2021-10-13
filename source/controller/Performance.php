84b10c5395a75039fad04e2ce5d46590d42f5393
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-10-13 12:22:46 +0530

    Add Performance.php

---
+++ source/controller/Performance.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PerformanceModel Controller
+ */
+class Performance extends Controller
+{
+	
+	function index()
+	{
+
+		// if(!Auth::logged_in())
+		// {
+		// 	$this->redirect('login');
+		// }
+		$user=new PerformanceModel();
+		$ar=Auth::user();
+		$row=array();
+		$row = $user->where('employee_ID',$ar);
+		$this->view('performance',['row'=>$row]);
+		
+	}
+}
+
