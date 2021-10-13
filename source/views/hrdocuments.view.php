4921ed9331c3c3641e691c1e54e435392594e706
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2021-10-13 20:37:14 +0530

    Update HR Documents Controller and View

--- source/views/hrdocuments.view.php
+++ source/views/hrdocuments.view.php
@@ -16,28 +16,13 @@
     <div>
    <?php $this->view('includes/header1')?>
 </div>
-
-<?php if(Auth::access('HR Manager')):?>
-<div>
-    <?php
-    $this->view('includes/hrmanagernavbar');
-    ?>
-</div> 
-<?php endif;?>
         <h1 class="h1">HR Documents</h1>
         <div class="row">
             <div class="column">
                 <div class="card">
                     <h4>Employee Application Form</h4>
                     <img src="<?= IMG_PATH?>hrdocuments/emp_application_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
                     <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-
                     <a id="link-1" href="<?= DOC_PATH ?>employment-application-form.docx" download hidden></a>
                 </div>
             </div>
@@ -45,14 +30,7 @@
                 <div class="card">
                     <h4>Employee Emergency Contact Form</h4>
                     <img src="<?= IMG_PATH?>hrdocuments/emp_emergency_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
                     <button class="btn" onclick="document.getElementById('link-2').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-
                     <a id="link-2" href="<?= DOC_PATH ?>employee-emergency-form.docx" download hidden></a>
                 </div>
             </div>
@@ -60,14 +38,7 @@
                 <div class="card">
                     <h4>Disciplinary action form</h4>
                     <img src="<?= IMG_PATH?>hrdocuments/emp_disciplinary_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
                     <button class="btn" onclick="document.getElementById('link-3').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-
                     <a id="link-3" href="<?= DOC_PATH ?>disciplinary-action-form.docx" download hidden></a>
                 </div>
             </div>
@@ -77,45 +48,24 @@
             <div class="column">
                 <div class="card">
                     <h4>HR Service Request Form</h4>
-                    <img src="<?= IMG_PATH?>hrdocuments/emp_hr_request_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
+                    <img src="<?= IMG_PATH?>hrdocuments/emp_hr_request_form.png" alt="" style="width:100%">                  
                     <button class="btn" onclick="document.getElementById('link-4').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-
                     <a id="link-4" href="<?= DOC_PATH ?>hr-service-request-form.docx" download hidden></a>
                 </div>
             </div>
             <div class="column">
                 <div class="card">
                     <h4>Exit Interview Form</h4>
-                    <img src="<?= IMG_PATH?>hrdocuments/emp_exit_interview_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
-                    <button class="btn" onclick="document.getElementById('link-5').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-                
-
-                    <a id="link-5" href="<?= DOC_PATH ?>exit-interview-form.docx" download hidden></a>
+                    <img src="<?= IMG_PATH?>hrdocuments/emp_exit_interview_form.png" alt="" style="width:100%">                   
+                    <button class="btn" onclick="document.getElementById('link-5').click()"><i class="fa fa-download"></i>      Download</button>           
+                   <a id="link-5" href="<?= DOC_PATH ?>exit-interview-form.docx" download hidden></a>
                 </div>
             </div>
             <div class="column">
                 <div class="card">
                     <h4>Travel Request Form</h4>
-                    <img src="<?= IMG_PATH?>hrdocuments/emp_travel_request_form.png" alt="" style="width:100%">
-                    <?php if(Auth::access('Employee')):?>
+                    <img src="<?= IMG_PATH?>hrdocuments/emp_travel_request_form.png" alt="" style="width:100%">                    
                     <button class="btn" onclick="document.getElementById('link-6').click()"><i class="fa fa-download"></i>      Download</button>
-                    <?php endif;?>
-
-                    <?php if(Auth::access('HR Manager')):?>
-                    <button class="btn" onclick="document.getElementById('link-1').click()"><i class="fa fa-download"></i>      Update</button>
-                    <?php endif;?>
-
                     <a id="link-6" href="<?= DOC_PATH ?>travel-request-form.docx" download hidden></a>
                 </div>
             </div>
