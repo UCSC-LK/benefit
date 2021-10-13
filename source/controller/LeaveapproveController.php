0566842eefbabce1532c0f61d66dbce4f58cb839
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2021-10-13 16:34:46 +0530

    Update Leave Approve Files/

--- source/controller/LeaveapproveController.php
+++ source/controller/LeaveapproveController.php
@@ -3,6 +3,11 @@
 class LeaveapproveController extends Controller{
 
   function index(){
+
+    if(!Auth::logged_in())
+		{
+			$this->redirect('login');
+		}
     $this->view('leaveapprove');
   }
 }
