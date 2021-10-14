9b8697f1b8845a6676d2ae4b4ff8cdff33eac2e1
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-10-15 02:35:59 +0530

    Update PerformanceModel.php

--- source/models/PerformanceModel.php
+++ source/models/PerformanceModel.php
@@ -6,40 +6,40 @@ class PerformanceModel extends Model
 {
 
 	protected $table = "performance";
-
 	
-	/*public function validate($DATA)
+	
+	public function validate($DATA)
 	{
-		$this->$errors=array();
+		$this->errors = array();
 		//check for commu
-		$communications=[0,25,50,75,100];
-		if(empty($DATA['communication'])|| in_array($DATA['communication'], $communications))
+		$communications=[1,25,50,75,100];
+		if(empty($DATA['communication'])|| !in_array($DATA['communication'], $communications))
 		{
-			$this->errors['communication']='communication is not valid';
+			$this->errors['communication']="Communication is not valid";
 		}
 		//check for quality_of_work
-		$quality_of_works=[0,25,50,75,100];
-		if(empty($DATA['quality_of_work'])|| in_array($DATA['quality_of_work'], $quality_of_works))
+		$quality_of_works=[1,25,50,75,100];
+		if(empty($DATA['quality_of_work'])|| !in_array($DATA['quality_of_work'], $quality_of_works))
 		{
-			$this->errors['quality_of_work']='quality_of_work is not valid';
+			$this->errors['quality_of_work']="Quality of work is not valid";
 		}
 		//check for organization
-		$organizations=[0,25,50,75,100];
-		if(empty($DATA['organization'])|| in_array($DATA['organization'], $organizations))
+		$organizations=[1,25,50,75,100];
+		if(empty($DATA['organization'])|| !in_array($DATA['organization'], $organizations))
 		{
-			$this->errors['organization']='organization is not valid';
+			$this->errors['organization']="organization is not valid";
 		}
 		//check for team_skills
-		$team_skills=[0,25,50,75,100];
-		if(empty($DATA['team_skills'])|| in_array($DATA['team_skills'], $team_skills))
+		$team_skillss=[1,25,50,75,100];
+		if(empty($DATA['team_skills'])|| !in_array($DATA['team_skills'], $team_skillss))
 		{
-			$this->errors['team_skills']='team_skills is not valid';
+			$this->errors['team_skills']="Team skills is not valid";
 		}
-		//check for commu
-		$multitasking_abilitys=[0,25,50,75,100];
-		if(empty($DATA['multitasking_ability'])|| in_array($DATA['multitasking_ability'], $multitasking_abilitys))
+		//check for multi
+		$multitasking_abilitys=[1,25,50,75,100];
+		if(empty($DATA['multitasking_ability'])|| !in_array($DATA['multitasking_ability'], $multitasking_abilitys))
 		{
-			$this->errors['multitasking_ability']='multitasking_ability is not valid';
+			$this->errors['multitasking_ability']="Multitasking ability is not valid";
 		}
 
 		if(count($this->errors)==0)
@@ -48,5 +48,5 @@ class PerformanceModel extends Model
 		}
 
 		return false;
-	}*/
+	}
 }
