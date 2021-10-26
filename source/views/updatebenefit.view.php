29a27604eb62568957aff41462091cce15961fe1
LakshaniNadeesha <2019cs105@stu.ucsc.lk>
2021-10-26 19:09:55 +0530

    Add Update Documents and Benefit

--- source/views/updatebenefit.view.php
+++ source/views/updatebenefit.view.php
@@ -35,22 +35,60 @@
     <?php endif; ?>
 
     <div class="main_container">
-        <div>
-            <p class="handling_title">Update Benefits</p>
+    <div class="benefit_head">
+            <fieldset>
+                <legend>UPDATE BENEFIT</legend>
+            <!-- <div class="heading">
+                    <h2>CLAIM BENEFIT</h2>
+                </div> -->
+
+                <div class="benefit_form">
+
+                    <form name="myform" action="#" method="post">
+
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="benefit_type">Benefit Type</label>
+                            </div>
+                            <div class="column_2">
+                                <select id="benefit_type" name="benefit_type" required>
+                                    <option></option>
+                                    <option>Medical Insurance</option>
+                                    <option>Life Insurance</option>
+                                    <option>Accident Insurance</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="claiming_amount">Maximum Amount</label>
+                            </div>
+                            <div class="column_2">
+                                <input type="text" id="claiming_amount" name="claiming_amount" placeholder="Rs.200,000.00" required pattern="[0-9._%+-]+\.[0-9]{2}$">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="column_1">
+                                <label for="claiming_date">Renew Date</label>
+                            </div>
+                            <div class="column_2">
+                            <input type="date" id="claim_date" name="claim_date" placeholder="mm/dd/yyyy" required>
+                        </div>
+                        </div>
+
+                        <div class="claim_button">
+                            <input type="submit" value="Update" name="submit">
+                            <a href="<?=PATH?>/Benefit/update">
+			 		        <input class="cancle_button" type="button" value="Cancel"></a>
+                        </div>
+
+                    </form>
+                    </fieldset>
+
+                </div>
+            </div>
         </div>
-
-    </div>
-</div>
-
-<script>
-
-    function addBox() {
-        var temp = document.getElementById("temp").content;
-        document.getElementById("btn").appendChild(temp);
-    }
-
-    document.getElementById("btn").addEventListener("click", addBox);
-
-</script>
 </body>
 </html>
