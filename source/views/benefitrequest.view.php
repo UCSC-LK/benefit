c214f27f1f3f503dd07c1e3794dc26423ab3b414
Sathya <sathyaudayangi2@gmail.com>
2021-10-27 23:40:21 +0530

    Add Back Button For Benefits And Leave Requesting Forms

--- source/views/benefitrequest.view.php
+++ source/views/benefitrequest.view.php
@@ -66,107 +66,111 @@
 
         <div class="benefit_container">
             <div class="benefit_head">
-            <fieldset>
-                <legend>CLAIM BENEFIT</legend>
-            <!-- <div class="heading">
-                    <h2>CLAIM BENEFIT</h2>
-                </div> -->
-
-                <div class="benefit_form">
-
-                    <form name="myform" action="BenefitrequestController" method="post"
-                          onsubmit=" return number_validation()">
-
-                        <div class="row">
-                            <div class="column_1">
-                                <label for="benefit_type">Benefit Type</label>
-                            </div>
-                            <div class="column_2">
-                                <select id="benefit_type" name="benefit_type" required>
-                                    <option></option>
-                                    <option>Medical Insurance</option>
-                                    <option>Life Insurance</option>
-                                    <option>Accident Insurance</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="column_1">
-                                <label for="claiming_date">Date</label>
-                            </div>
-                            <div class="column_2">
-                                <input type="date" id="claiming_date" value="<?php echo date('Y-m-d') ?>"
-                                       name="claiming_date" placeholder="mm/dd/yyyy" readonly>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="column_1">
-                                <label for="claiming_amount">Claiming Amount</label>
-                            </div>
-                            <div class="column_2">
-                                <input type="text" id="claiming_amount" name="claiming_amount"
-                                       placeholder="Rs.20,000.00" required pattern="[0-9._%+-]+\.[0-9]{2}$">
-                                <span id="numberText"></span>
-                            </div>
-                        </div>
-
-
-                        <div class="row">
-                            <div class="column_1">
-                                <label for="subject">Reason</label>
-                            </div>
-                            <div class="column_2">
-                                    <textarea id="subject" name="subject" placeholder="Why You Are Applying..." style="height:100px;" required></textarea>
-                                <span id="reasonText"></span>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="column_1">
-                                <label for="submission">Report Submission</label>
-                            </div>
-
-                            <div class="report_submission">
-                                <input type="file" id="report_submission" name="report_submission"
-                                       accept=".pdf, .png" multiple required>
-                                <span id="custom-text"><div class="file_text">No file chosen, yet....</div></span>
-                                <span id="upload"><div class="upload"><a href="#">Upload Here</a></div></span>
-                            </div>
-                        </div>
-
-                        <div class="claim_button">
-                            <input type="submit" value="Apply" name="submit">
-                            <a href="<?=PATH?>/Benefit">
-			 		        <input class="cancle_button" type="button" value="Cancel"></a>
-                        </div>
-
-                        <script type="text/javascript">
-                            const report_submission = document.getElementById('report_submission');
-                            const custom_text = document.getElementById('custom_text');
-                            const upload = document.getElementById('upload');
-
-                            upload.addEventListener("click", function () {
-                                report_submission.click();
-                            });
-
-                            report_submission.addEventListener("click", function () {
-                                if (report_submission.value) {
-                                    custom_text.innerHTML = report_submission.value;
-                                } else {
-                                    custom_text.innerHTML = "No File chose, yet";
-                                }
-                            });
-
-                        </script>
-                    </form>
-                    </fieldset>
-
+                <div class="back_btn">
+                    <a href="Benefit"><i class="large material-icons">arrow_back</i></a>
                 </div>
+                <fieldset>
+                    <legend>CLAIM BENEFIT</legend>
+                    <!-- <div class="heading">
+                            <h2>CLAIM BENEFIT</h2>
+                        </div> -->
+
+                    <div class="benefit_form">
+
+                        <form name="myform" action="BenefitrequestController" method="post"
+                              onsubmit=" return number_validation()">
+
+                            <div class="row">
+                                <div class="column_1">
+                                    <label for="benefit_type">Benefit Type</label>
+                                </div>
+                                <div class="column_2">
+                                    <select id="benefit_type" name="benefit_type" required>
+                                        <option></option>
+                                        <option>Medical Insurance</option>
+                                        <option>Life Insurance</option>
+                                        <option>Accident Insurance</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="column_1">
+                                    <label for="claiming_date">Date</label>
+                                </div>
+                                <div class="column_2">
+                                    <input type="date" id="claiming_date" value="<?php echo date('Y-m-d') ?>"
+                                           name="claiming_date" placeholder="mm/dd/yyyy" readonly>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="column_1">
+                                    <label for="claiming_amount">Claiming Amount</label>
+                                </div>
+                                <div class="column_2">
+                                    <input type="text" id="claiming_amount" name="claiming_amount"
+                                           placeholder="Rs.20,000.00" required pattern="[0-9._%+-]+\.[0-9]{2}$">
+                                    <span id="numberText"></span>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="column_1">
+                                    <label for="subject">Reason</label>
+                                </div>
+                                <div class="column_2">
+                                    <textarea id="subject" name="subject" placeholder="Why You Are Applying..."
+                                              style="height:100px;" required></textarea>
+                                    <span id="reasonText"></span>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="column_1">
+                                    <label for="submission">Report Submission</label>
+                                </div>
+
+                                <div class="report_submission">
+                                    <input type="file" id="report_submission" name="report_submission"
+                                           accept=".pdf, .png" multiple required>
+                                    <span id="custom-text"><div class="file_text">No file chosen, yet....</div></span>
+                                    <span id="upload"><div class="upload"><a href="#">Upload Here</a></div></span>
+                                </div>
+                            </div>
+
+                            <div class="claim_button">
+                                <input type="submit" value="Apply" name="submit">
+                                <a href="<?= PATH ?>/Benefit">
+                                    <input class="cancle_button" type="button" value="Cancel"></a>
+                            </div>
+
+                            <script type="text/javascript">
+                                const report_submission = document.getElementById('report_submission');
+                                const custom_text = document.getElementById('custom_text');
+                                const upload = document.getElementById('upload');
+
+                                upload.addEventListener("click", function () {
+                                    report_submission.click();
+                                });
+
+                                report_submission.addEventListener("click", function () {
+                                    if (report_submission.value) {
+                                        custom_text.innerHTML = report_submission.value;
+                                    } else {
+                                        custom_text.innerHTML = "No File chose, yet";
+                                    }
+                                });
+
+                            </script>
+                        </form>
+                </fieldset>
+
             </div>
         </div>
     </div>
+</div>
 </div>
 
 <!--<div>-->
