68ca45f60d2c074038229f2caa3e3691554ca1d9
Dilukshan Bimsara <dilukshanbimsara@gmail.com>
2021-10-29 23:59:46 +0530

    Update requestleve Files

--- source/views/requestleave.view.php
+++ source/views/requestleave.view.php
@@ -45,64 +45,67 @@
                         <!-- <div class="heading">
                             <h2>REQUEST LEAVES</h2>
                         </div> -->
-                        <div class="leave_type">
-                            <label for="leave_type">Select Leave Type</label>
-                            <select name="leave_type" id="leave_type">
-                                <option value="" id="hide"></option>
-                                <option value="sick">Sick Leave</option>
-                                <option value="casual">Casual Leave</option>
-                                <option value="annual">Annual Leave</option>
-                            </select>
-                        </div>
-
-                        <div class="full_day">
-                            <p>Full Days</p>
-                            <div class="full_duration">
-                                <div class="start_date">
-                                    <label for="start_date">Start Date </label>
-                                    <input type="date" name="start_date" id="start_date" min="2021-08-01" max="">
-                                </div>
-
-                                <div class="end_date">
-                                    <label for="end_date">End Date</label>
-                                    <input type="date" name="end_date" id="end_date" min="" max="">
-                                </div>
+                        <div class="data">
+                            <div class="leave_type">
+                                <label for="leave_type">Leave Type</label>
+                                <select name="leave_type" id="leave_type">
+                                    <option value="" id="hide"></option>
+                                    <option value="sick">Sick Leave</option>
+                                    <option value="casual">Casual Leave</option>
+                                    <option value="annual">Annual Leave</option>
+                                </select>
                             </div>
 
-                        </div>
+                            <div class="full_day">
+                                <p>Full Days</p>
+                                <div class="full_duration">
+                                    <div class="start_date">
+                                        <label for="start_date">Start Date </label>
+                                        <input type="date" name="start_date" id="start_date" min="2021-08-01" max="">
+                                    </div>
 
-                        <div class="half_day">
-                            <p>Half Days</p>
-                            <div class="half_duration">
-                                <div class="date_item">
-                                    <label for="start_date">Date </label>
-                                    <input type="date" name="start_date" id="half_date" min="" max="">
+                                    <div class="end_date">
+                                        <label for="end_date">End Date</label>
+                                        <input type="date" name="end_date" id="end_date" min="" max="">
+                                    </div>
                                 </div>
 
-                                <div class="item">
-                                    <div class="item-1">
-                                        <label for="half">Morning </label>
-                                        <input type="radio" id="half" name="half" value="morning">
+                            </div>
+
+                            <div class="half_day">
+                                <p>Half Days</p>
+                                <div class="half_duration">
+                                    <div class="date_item">
+                                        <label for="start_date">Date </label>
+                                        <input type="date" name="start_date" id="half_date" min="" max="">
                                     </div>
-                                    <div class="item-1">
+
+                                    <div class="item">
+                                        <div class="item-1">
+                                            <label for="half">Morning </label>
+                                            <input type="radio" id="half" name="half" value="morning">
+                                        </div>
+                                        <div class="item-1">
+                                            <label for="half">Evening</label>
+                                            <input type="radio" name="half" value="evening" id="half">
+                                        </div>
+                                    </div>
+
+                                    <!-- <div class="item">
                                         <label for="half">Evening</label>
                                         <input type="radio" name="half" value="evening" id="half">
-                                    </div>
-                                </div>
+                                    </div> -->
 
-                                <!-- <div class="item">
-                                    <label for="half">Evening</label>
-                                    <input type="radio" name="half" value="evening" id="half">
-                                </div> -->
+                                </div>
+                            </div>
+
+                            <div class="buttons">
+                                <button type="submit" id="request" name="submit" class="request-btn">Request</button>
+                                <button type="reset" id="cancel" class="cancle-btn">Cancel</button>
 
                             </div>
                         </div>
-
-                        <div class="buttons">
-                            <button type="submit" id="request" name="submit" class="request-btn">Request</button>
-                            <button type="reset" id="cancel" class="cancle-btn">Cancel</button>
-
-                        </div>
+                        
                     </form>
                 </fieldset>
             </div>
