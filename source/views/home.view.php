6a52f45f3b9ddf9b711e3d7f2e9bd623292ce811
Sathya <sathyaudayangi2@gmail.com>
2021-10-30 01:10:18 +0530

    Add Icon To Home Cards

--- source/views/home.view.php
+++ source/views/home.view.php
@@ -24,8 +24,8 @@
         <div class="card_container" id = "financial">
             <div class="card">
                 <h2>Financial Branch</h2>
-                <p>Manger: Mr.Aruna</p>
-                <p>Employee: 50</p>
+                <p id="manager">Manager: Mr.Aruna</p>
+                <p><i class="fa fa-users" aria-hidden="true"></i>12</p>
                 <button class="show-more">Show More <i class="fas fa-arrow-right"></i></button>
             </div>
         </div>
@@ -33,8 +33,8 @@
         <div class="card_container" id = "financial">
             <div class="card">
                 <h2><b>HRM Branch</b></h2>
-                <p>Manger: Mr.Silva</p>
-                <p>Employee: 50</p>
+                <p id="manager">Manager: Mr.Silva</p>
+                <p><i class="fa fa-users" aria-hidden="true"></i> 10</p>
                 <button class="show-more">Show More <i class="fas fa-arrow-right"></i></button>
             </div>
         </div>
@@ -42,8 +42,8 @@
         <div class="card_container" id = "financial">
             <div class="card">
                 <h2>Operational Branch</h2>
-                <p>Manger: Mr.Chathura</p>
-                <p>Employee: 50</p>
+                <p id="manager">Manager: Mr.Chathura</p>
+                <p><i class="fa fa-users" aria-hidden="true"></i> 33</p>
                 <button class="show-more">Show More <i class="fas fa-arrow-right"></i></button>
             </div>
         </div>
@@ -51,8 +51,8 @@
         <div class="card_container" id = "financial">
             <div class="card">
                 <h2>Selling Branch</h2>
-                <p>Manger: Mr.Bimsara</p>
-                <p>Employee: 50</p>
+                <p id="manager">Manager: Mr.Bimsara</p>
+                <p><i class="fa fa-users" aria-hidden="true"></i> 27</p>
                 <button class="show-more">Show More <i class="fas fa-arrow-right"></i></button>
             </div> 
         <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L30,229.3C60,203,120,149,180,106.7C240,64,300,32,360,26.7C420,21,480,43,540,58.7C600,75,660,85,720,80C780,75,840,53,900,37.3C960,21,1020,11,1080,53.3C1140,96,1200,192,1260,234.7C1320,277,1380,267,1410,261.3L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg> -->
