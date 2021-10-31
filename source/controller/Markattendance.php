7224f2c534791c74ebe04bf6be39d96a4c3fb7d9
CHATHURALIYANAGE1997 <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2021-10-31 23:52:23 +0530

    Update Markattendance.php

--- source/controller/Markattendance.php
+++ source/controller/Markattendance.php
@@ -14,12 +14,12 @@ class Markattendance extends Controller
 			$this->redirect('login');
 		}
 		
-		
+		if(Auth::access('Supervisor'))
+   		 {
 		$this->view('markattendance');
-		
 	}
-
-	
-
-
+	else{
+    $this->view('404');
+  	}	
+	}
 }
