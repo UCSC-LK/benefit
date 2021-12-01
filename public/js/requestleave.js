3668394e1324b1cc93c30a9dcc149666dd282365
Dilukshan Bimsara <dilukshanbimsara@gmail.com>
2021-12-01 22:47:52 +0530

    Check Database Connection In Request Leave

--- public/js/requestleave.js
+++ public/js/requestleave.js
@@ -15,6 +15,9 @@ var max_m = dateObj.getUTCMonth()+1;
 var max_d = dateObj.getUTCDate();
 var max_y = dateObj.getUTCFullYear();
 
+max_m = setDecimal(max_m);
+max_d = setDecimal(max_d);
+
 today_date = max_y + "-" + max_m + "-" + max_d;
 console.log(today_date);
 
@@ -23,18 +26,14 @@ leave_type.addEventListener('input',()=>{
     console.log("inside Event Listener ");
 
     if(leave_type.value == "sick"){
-        // var dateObj = new Date();
-        var min_date = subDays(dateObj,5);
-        console.log(`${min_date}=min date`);
 
+        var min_date = subDays(dateObj,5);
         var min_m = min_date.getUTCMonth() + 1; //months from 1-12
         var min_d = min_date.getUTCDate();
         var min_y = min_date.getUTCFullYear();
 
-        if(number.indexOf(min_d) != -1){
-            min_d = "0" + min_d;
-            console.log(`inside decimal point if condition ${min_d}`);
-        }
+        min_m = setDecimal(min_m);
+        min_d = setDecimal(min_d);
 
         newdate = min_y + "-" + min_m + "-" + min_d;
 
@@ -42,37 +41,27 @@ leave_type.addEventListener('input',()=>{
         var max_d = dateObj.getUTCDate();
         var max_y = dateObj.getUTCFullYear();
 
-        if(number.indexOf(max_d) != -1){
-            max_d = "0" + max_d;
-            console.log(`inside decimal point if condition ${max_d}`);
-        }
+        max_m = setDecimal(max_m);
+        max_d = setDecimal(max_d);
 
         max_date = max_y + "-" + max_m + "-" + max_d;
-        
     
-        console.log(`${newdate} = start date min in sick leave`);
-        console.log(`${max_date} =  max date`);
         start_date.min = newdate;
         start_date.max = max_date;
-        console.log(`${start_date.max} = start date max in sick leave`);
+        // console.log(`${start_date.max} = start date max in sick leave`);
 
         start_date.addEventListener('input',()=>{
 
             var end_date = document.getElementById("end_date");
             end_date.min = start_date.value;
-            console.log(start_date);
+            // console.log(start_date);
             end_date.max = max_date;
             
-            console.log(`${end_date.min} end date min`);
-            console.log(`${end_date.max} end date max`);
+            // console.log(`${end_date.min} end date min`);
+            // console.log(`${end_date.max} end date max`);
             // today accident and full week in hospital senario have to discuss
         })
         
-
-        // console.log(start_date.value);
-        // console.log(start_date.min);
-
-        // console.log("inside if condition ");
     }
     else if(leave_type.value == "casual"){
         start_date.min = today_date;
@@ -83,17 +72,20 @@ leave_type.addEventListener('input',()=>{
         var max_d = max_date.getUTCDate();
         var max_y = max_date.getUTCFullYear();
 
+        max_m = setDecimal(max_m);
+        max_d = setDecimal(max_d);
+
         max_date = max_y + "-" + max_m + "-" + max_d;
         
         start_date.max = max_date;
 
         start_date.addEventListener('input',()=>{
             var end_date = document.getElementById("end_date");
-            console.log(start_date.value);
+           
             end_date.min = start_date.value;
             end_date.max = max_date;
-            console.log(`${end_date.min} end date min`);
-            console.log(`${end_date.max} end date max`);
+            // console.log(`${end_date.min} end date min`);
+            // console.log(`${end_date.max} end date max`);
             // today accident and full week in hospital senario have to discuss
         })
         
@@ -107,16 +99,16 @@ leave_type.addEventListener('input',()=>{
         var max_d = max_date.getUTCDate();
         var max_y = max_date.getUTCFullYear();
 
+        max_m = setDecimal(max_m);
+        max_d = setDecimal(max_d);
+
         max_date = max_y + "-" + max_m + "-" + max_d;
         
         start_date.max = max_date;
         start_date.addEventListener('input',()=>{
             var end_date = document.getElementById("end_date");
-            console.log(start_date.value);
             end_date.min = start_date.value;
             end_date.max = max_date;
-            console.log(`${end_date.min} end date min`);
-            console.log(`${end_date.max} end date max`);
             // today accident and full week in hospital senario have to discuss
         })
         
@@ -130,7 +122,14 @@ function addDays(myDate,days) {
 function subDays(myDate, days){
     return new Date(myDate.getTime() - days*24*60*60*1000);
 }
-    
+
+function setDecimal(val){
+    if(number.indexOf(val) != -1){
+        val = "0" + val;
+        console.log(`inside decimal point if condition ${val}`);
+    }
+    return val;
+}
 var myDate = new Date('2013-02-11');
     
 
--- source/controller/RequestleaveController.php
+++ source/controller/RequestleaveController.php
@@ -10,20 +10,20 @@ class RequestleaveController extends Controller{
 
             if(isset($_POST['submit']))
             {
+                $arr['employee_ID'] = Auth::user();
                 $arr['leave_type'] = $_POST['leave_type'];
-                $arr['start_date'] = $_POST['start_date'];
-                $arr['end_date'] = $_POST['end_date'];
-                $arr['half'] = $_POST['half'];
-                $arr['half_date'] = $_POST['half_date'];
+                $arr['starting_date'] = $_POST['start_date'];
+                $arr['ending_date'] = $_POST['end_date'];
                 $arr['leave_status'] = "Pending";
-
+                $arr['half_date'] = $_POST['half_date'];
+                $arr['half_time'] = $_POST['half_time'];
                 if($arr['start_date'] == null && $arr['half_date'] != null){
                     $arr['start_date'] = $_POST['half_date'];
                     $arr['end_date'] = $_POST['half_date'];
                 }
-                
+                $user->insert($arr);
             }
-            $this->view('requestleave',['rows'=>$arr]);
+            $this->view('requestleave');
         }else{
             $this->view('requestleave');
         }      
